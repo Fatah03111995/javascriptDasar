969e2e6e44ace77c20dd56abfad4dddb9aafb69d
Abdul Fatah <103168830+Fatah03111995@users.noreply.github.com>
2022-12-22 09:42:37 +0700

    Add files via upload

---
+++ ErrorHandling/Err.js
@@ -0,0 +1,23 @@
+let json = '{ "name": Yoda", "age": 20 }';
+ 
+try {
+    let user = JSON.parse(json);
+ 
+    if (!user.name) {
+        throw new SyntaxError("'name' is required.");
+    }
+ 
+    console.log(user.name); // Yoda
+    console.log(user.age);  // 20
+} catch (error) {
+        if (error instanceof SyntaxError) {
+            console.log(`JSON Error: ${error.message}`);
+        } else if (error instanceof ReferenceError) {
+            console.log(error.message);
+        } else {
+            console.log(error.stack);
+        }
+} finally{
+    console.log("finally dijalankan")
+}
+ 
